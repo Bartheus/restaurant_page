961ab0f026091ccaa1f6acc7558fe4839c0e0f32
Bartheus <bartslapinski@gmail.com>
2021-03-02 10:47:14 +0100

    add vuex data and component template

---
+++ src/views/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <template>
+    <div class="home">
+      <ul class="posts">
+        <li v-for="burger in posts" :key="burger.name" class="burger-item">
+          <h1>{{ burger.name }}</h1>
+        </li>
+      </ul>
+    </div>
+  </template>
+</template>
+
+<script>
+export default {
+  computed: {
+    burgers() {
+      return this.$store.state.burgers
+    }
+  }
+}
+</script>
+
+<style></style>
