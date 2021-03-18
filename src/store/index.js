09457ea6bb3c3d95c3a3d297dc620cdbc0a7b590
Bartheus <bartslapinski@gmail.com>
2021-03-18 10:26:52 +0100

    rebasing to develop

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     burgers: [
+<<<<<<< HEAD
       { name: 'Burger 1' },
       { name: 'Burger 2' },
       { name: 'Burger 3' },
@@ -15,6 +16,18 @@ export default new Vuex.Store({
       { name: 'Burger 7' },
       { name: 'Burger 8' },
       { name: 'Burger 9' }
+=======
+      { name: 'Burger1' },
+      { name: 'Burger2' },
+      { name: 'Burger3' },
+      { name: 'Burger4' },
+      { name: 'Burger5' },
+      { name: 'Burger6' },
+      { name: 'Burger7' },
+      { name: 'Burger8' },
+      { name: 'Burger9' },
+      { name: 'Burger10' }
+>>>>>>> 03dfda6 (add data in vuex state)
     ]
   },
   mutations: {},
